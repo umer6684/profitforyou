11507e43f21d623d8d514aee9813053c5763dc84
umer6684 <umerarshad6633@gmail.com>
2025-04-14 01:53:15 +0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Example: Script to handle registration form submission
-document.getElementById('registerForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Form Submitted!');
-});
