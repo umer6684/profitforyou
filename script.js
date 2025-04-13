776f6272a988622dd039a9c319aa6840ab255e25
umer6684 <umerarshad6633@gmail.com>
2025-04-14 00:44:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Script to handle registration form submission
+document.getElementById('registerForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Form Submitted!');
+});
